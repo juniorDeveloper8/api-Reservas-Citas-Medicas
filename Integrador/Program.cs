06721f6b98115cd9581f6879b317d07dbfd5a790
juniorDeveloper8 <be6323787@gmail.com>
2024-02-05 23:04:47 -0500

    configuracion de produccion

--- Integrador/Program.cs
+++ Integrador/Program.cs
@@ -13,13 +13,12 @@ var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 // Agregar servicios al contenedor.
 builder.Services.AddControllers();
-builder.Services.AddControllers();
 
 // Configuración de la conexión de base de datos
 builder.Services.AddDbContext<DbIntegradorContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-/**CONFIGURACION DE CORS**/
+// CONFIGURACION DE CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowSpecificOrigins,
@@ -30,10 +29,11 @@ builder.Services.AddCors(options =>
                           .AllowAnyMethod();
                       });
 });
+
 // Registrar DbIntegradorContextProcedures
 builder.Services.AddScoped<DbIntegradorContextProcedures>();
 
-// MIS SERVICIO
+// MIS SERVICIOS
 /************************************************************/
 builder.Services.AddScoped<UserServices>();
 builder.Services.AddScoped<InsertUserService>();
@@ -60,12 +60,10 @@ builder.Services.AddScoped<UpdateClinicaService>();
 builder.Services.AddScoped<InsertClinicaService>();
 builder.Services.AddScoped<DeleteClinicaService>();
 
-//toca corregir
+// TOCA CORREGIR
 builder.Services.AddScoped<ListFichaService>();
 builder.Services.AddScoped<InsertFichaService>();
 /************************************************************/
-
-
 
 // Añadir el servicio de documentación de Swagger
 builder.Services.AddEndpointsApiExplorer();
@@ -74,7 +72,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configurar el pipeline de solicitudes HTTP.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
@@ -82,6 +80,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
+app.UseCors(MyAllowSpecificOrigins); // Agregar CORS
 app.MapControllers();
 
 app.Run();
